f378616bd6653f2490a0518d6c9ede99c1f6989a
Barry Melton <barry.melton@gmail.com>
2012-04-11 11:29:02 -0400

    Busted the template pulling the analytics out.  Oops

--- orbvt.js
+++ orbvt.js
@@ -7,9 +7,9 @@ if (Meteor.is_client) {
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    if(Posts.find().count() === 0) { 
-      var post = {title: 'Test post', author: 'bmelton', content: 'This is the first test on our somewhat ridiculous Svbtle clone that we built with the significantly less ridiculous <a href="http://meteor.com/" target="_blank">Meteor application framework</a>.',date: 'April 11'};
-      Posts.insert(post);
-     }
+    // if(Posts.find().count() === 0) { 
+      // var post = {title: 'Test post', author: 'bmelton', content: 'This is the first test on our somewhat ridiculous Svbtle clone that we built with the significantly less ridiculous <a href="http://meteor.com/" target="_blank">Meteor application framework</a>.',date: 'April 11'};
+      // Posts.insert(post);
+     // }
   });
 }
