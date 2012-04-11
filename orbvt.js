f74ca5306361b921884742cc56d4fc6b2408bfcf
Barry Melton <barry.melton@gmail.com>
2012-04-11 13:24:30 -0400

    Bulletproofed the 'test post'

--- orbvt.js
+++ orbvt.js
@@ -1,4 +1,6 @@
 Posts = new Meteor.Collection("Orbvt");
+default_message = "This is the first post on the Orbvt (and cannot be destroyed), the somewhat ridiculous Svbtle clone built on the significantly less ridiculous Meteor application framework.  The admin console is your Javascript console.  To create a new message, try this: Posts.insert({title: 'Example Post', author: 'your name', date: 'Today', content: 'This is an example post!'});"
+
 if (Meteor.is_client) {
   Template.orbvt.posts = function() { 
     return Posts.find();
@@ -7,11 +9,15 @@ if (Meteor.is_client) {
   Template.orbvt.events = {
     'click h1': function(event) { 
       event.preventDefault();
-      console.log(this);
     },
     'click .destroy': function (event) {
       console.log(event.currentTarget.id);
       Posts.remove(event.currentTarget.id);
+      if(Posts.find().count() == 0) { 
+        console.log("Empty!");
+        var post = {title: 'Test post', author: 'bmelton', content: default_message, date: 'April 11'};
+        Posts.insert(post);
+      }
     },
   }
 }
@@ -19,7 +25,7 @@ if (Meteor.is_client) {
 if (Meteor.is_server) {
   Meteor.startup(function () {
     if(Posts.find().count() === 0) { 
-      var post = {title: 'Test post', author: 'bmelton', content: 'This is the first test on our somewhat ridiculous Svbtle clone that we built with the significantly less ridiculous <a href="http://meteor.com/" target="_blank">Meteor application framework</a>.',date: 'April 11'};
+      var post = {title: 'Test post', author: 'bmelton', content: default_message, date: 'April 11'};
       Posts.insert(post);
      }
   });
