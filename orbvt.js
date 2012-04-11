2b8b8a872a2c512cc337c72e56510ca564fc8a87
Barry Melton <barry.melton@gmail.com>
2012-04-11 13:12:51 -0400

    Added support for removing elements.

--- orbvt.js
+++ orbvt.js
@@ -3,13 +3,24 @@ if (Meteor.is_client) {
   Template.orbvt.posts = function() { 
     return Posts.find();
   };
+
+  Template.orbvt.events = {
+    'click h1': function(event) { 
+      event.preventDefault();
+      console.log(this);
+    },
+    'click .destroy': function (event) {
+      console.log(event.currentTarget.id);
+      Posts.remove(event.currentTarget.id);
+    },
+  }
 }
 
 if (Meteor.is_server) {
   Meteor.startup(function () {
-    // if(Posts.find().count() === 0) { 
-      // var post = {title: 'Test post', author: 'bmelton', content: 'This is the first test on our somewhat ridiculous Svbtle clone that we built with the significantly less ridiculous <a href="http://meteor.com/" target="_blank">Meteor application framework</a>.',date: 'April 11'};
-      // Posts.insert(post);
-     // }
+    if(Posts.find().count() === 0) { 
+      var post = {title: 'Test post', author: 'bmelton', content: 'This is the first test on our somewhat ridiculous Svbtle clone that we built with the significantly less ridiculous <a href="http://meteor.com/" target="_blank">Meteor application framework</a>.',date: 'April 11'};
+      Posts.insert(post);
+     }
   });
 }
